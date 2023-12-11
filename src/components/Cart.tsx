6c0a52533f629da03749cbec465c4d30d5452236
vikalp <avikalp233@gmail.com>
2023-12-11 17:21:01 +0530

    Cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,100 @@
+"use client";
+
+import { ShoppingCart } from "lucide-react";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import Image from "next/image";
+
+const itemCount = 0;
+const fee = 50;
+const total = 100;
+
 const Cart = () => {
     return (
-        <div></div>
+        <Sheet>
+            <SheetTrigger className="group -m-2 flex items-center p-2">
+                <ShoppingCart 
+                aria-hidden="true" 
+                className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"/>
+
+                <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+            </SheetTrigger>
+
+
+            <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+                <SheetHeader className="space-y-2.5 pr-6">
+                    <SheetTitle>Cart(0)</SheetTitle>
+                </SheetHeader>
+
+                {itemCount > 0 ? (
+                    <>
+                        <div className="flex w-full pr-6 flex-col">
+                            {/**Cart Logic */}
+                            Cart Items
+                        </div>
+
+                        <div className="space-y-4 pr-6">
+                            <Separator />
+                            <div className="space-y-1.5 text-sm">
+                                <div className="flex">
+                                    <span className="flex-1">Shipping</span>
+                                    <span>Free</span>
+                                </div>
+
+                                <div className="flex">
+                                    <span className="flex-1">Transaction Fee</span>
+                                    <span>{formatPrice(fee)}</span>
+                                </div>
+
+                                <div className="flex">
+                                    <span className="flex-1">Total</span>
+                                    <span>{formatPrice(total)}</span>
+                                </div>
+                            </div>
+
+                            <SheetFooter>
+                                <SheetTrigger asChild>
+                                    <Link href={"/cart"} className={buttonVariants({
+                                        className : "w-full"
+                                    })}>
+                                        Continue to Checkout
+                                    </Link>
+                                </SheetTrigger>
+                            </SheetFooter>
+                        </div>
+                    </>
+                ) : (
+                    <div className="flex h-full flex-col items-center justify-center space-y-1">
+                        <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+                            <Image 
+                            src={"/hippo-empty-cart.png"} 
+                            fill 
+                            alt="empty shopping cart"
+                            aria-hidden = "true"
+                            />
+                        </div>
+                        <div className="text-xl font-semibold">
+                            Your cart is empty
+                        </div>
+                        <SheetTrigger asChild>
+                            <Link 
+                            href={"/products"} 
+                            className={buttonVariants({
+                                variant : "link",
+                                size:"sm",
+                                className:"text-sm text-muted-foreground"
+                            })}>
+                                Add items to your cart to checkout
+                            </Link>
+                        </SheetTrigger>
+                    </div>
+                )}
+
+            </SheetContent>
+        </Sheet>
     )
 }
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(
+  price : number | string,
+  options : {
+    currency? :"USD" | "EUR" | "INR" | "GBP",
+    notation? :Intl.NumberFormatOptions["notation"],
+  } = {}
+){
+  const {currency = "INR" , notation = "compact"} = options
+  
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price
+
+  return new Intl.NumberFormat("en-US" , {
+    style : "currency",
+    currency : currency,
+    notation : notation,
+    maximumFractionDigits : 2
+  }).format(numericPrice);
+}
