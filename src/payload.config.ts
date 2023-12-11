911af990682dbf543972dccb725ab121cfb34023
vikalp <avikalp233@gmail.com>
2023-12-11 20:53:16 +0530

    Setting up out admin dashboard

---
+++ src/payload.config.ts
@@ -0,0 +1,31 @@
+import {buildConfig} from "payload/config";
+import {mongooseAdapter} from "@payloadcms/db-mongodb"
+import {slateEditor} from "@payloadcms/richtext-slate";
+import {webpackBundler} from "@payloadcms/bundler-webpack";
+import path from "path";
+
+export default buildConfig({
+    serverURL : process.env.NEXT_PUBLIC_SERVER_URL || "",
+    collections : [],
+    routes : {
+        admin : "/sell"
+    },
+    admin : {
+        bundler : webpackBundler(),
+        meta : {
+            titleSuffix : "- DigitalHippo",
+            favicon : "/favicon.ico",
+            ogImage : "/thumbnail.jpg"
+        }
+    },
+    rateLimit : {
+        max: 2000,
+    },
+    editor : slateEditor({}),
+    db: mongooseAdapter({
+        url : process.env.MONGODB_URL!,
+    }),
+    typescript : {
+        outputFile : path.resolve(__dirname,"payload-types.ts")
+    }
+})
