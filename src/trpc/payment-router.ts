f67e6fa2854e46e3628ed0a9c68049fdcdc6866e
vikalp <avikalp233@gmail.com>
2023-12-19 19:12:30 +0530

    Creating the thank you page

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { privateProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
 import { stripe } from "../lib/stripe";
@@ -48,7 +48,7 @@ export const paymentRouter = router({
         })
 
         line_items.push({
-            price: "price_1OOvFsSFzjGIsWdFzsprsGge",
+            price: "price_1OOwE7L0DqjZxjU9eovarSUu",
             quantity: 1,
             adjustable_quantity: {
                 enabled: false,
@@ -73,6 +73,33 @@ export const paymentRouter = router({
             console.log(error);
 
             return { url:null }
+        }
+    }),
+
+    pollOrderstatus : privateProcedure.input(z.object({orderId: z.string()}))
+    .query(async ({input}) => {
+        const { orderId } = input
+
+        const payload = await getPayloadClient();
+
+        const { docs: orders } = await payload.find({
+            collection: "orders",
+            where: {
+                id: {
+                    equals: orderId
+                }
+            }
+        })
+
+        if(!orders.length)
+        {
+            throw new TRPCError({code: "NOT_FOUND"})
+        }
+
+        const [order] = orders
+
+        return {
+            isPaid: order._isPaid
         }
     })
 })
