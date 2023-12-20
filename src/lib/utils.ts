948b6ed634c6ae3709828bcdf8f104d31e90f6b2
vikalp <avikalp233@gmail.com>
2023-12-20 12:09:11 +0530

    Fixing the metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
@@ -22,4 +23,48 @@ export function formatPrice(
     notation : notation,
     maximumFractionDigits : 2
   }).format(numericPrice);
+}
+
+
+export function constructMetadata({
+  title = 'DigitalHippo',
+  description = 'DigitalHippo is an open-source marketplace for high-quality digital goods.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@joshtriedcoding',
+    },
+    icons,
+    metadataBase: new URL('https://digitalhippo-production-6a3a.up.railway.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
