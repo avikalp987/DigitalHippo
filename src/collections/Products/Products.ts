5d4a80b9f6dba5cf0e4544bb4a8b4a7af0c35ad8
vikalp <avikalp233@gmail.com>
2023-12-20 10:22:29 +0530

    Final security tweaks

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,8 +1,9 @@
 import { BeforeChangeHook } from "payload/dist/collections/config/types";
 import { PRODUCT_CATEGORIES } from "../../config";
-import { CollectionConfig } from "payload/types";
-import { Product } from "../../payload-types";
+import { Access, CollectionConfig } from "payload/types";
+import { Product, User } from "../../payload-types";
 import { stripe } from "../../lib/stripe";
+import { AfterChangeHook } from "payload/dist/collections/config/types";
 
 const addUser: BeforeChangeHook<Product> = async ({req, data}) => {
     const user = req.user
@@ -13,14 +14,82 @@ const addUser: BeforeChangeHook<Product> = async ({req, data}) => {
     }
 }
 
+const syncUser: AfterChangeHook<Product> = async ({req, doc}) =>{
+    const fullUser = await req.payload.findByID({
+        collection: "users",
+        id: req.user.id
+    })
+
+    if(fullUser && typeof fullUser === "object")
+    {
+        const {products} = fullUser
+
+        const allIDs = [
+            ...(products?.map((product) => 
+            typeof product==="object" ? product.id : product) 
+            || []),
+        ]
+
+        const createdProductIDs = allIDs.filter((id, index) => allIDs.indexOf(id) === index)
+
+        const dataToUpdate = [
+            ...createdProductIDs, 
+            doc.id
+        ]
+
+        await req.payload.update({
+            collection: "users",
+            id: fullUser.id,
+            data: {
+                products: dataToUpdate,
+            }
+        })
+    }
+}
+
+const isAdminOrHasAccess = (): Access => ({req: {user: _user}}) =>{
+    const user = _user as User | undefined
+
+    if(!user)
+    return false
+
+    if(user.role === "admin")
+    return true
+
+    const userProductIDs = (user.products || []).reduce<Array<string>>((acc, product) => {
+        if(!product) return acc
+
+        if(typeof product === "string") {
+            acc.push(product)
+        }
+        else
+        {
+            acc.push(product.id)
+        }
+
+        return acc
+    },[]) 
+
+    return {
+        id: {
+            in: userProductIDs
+        }
+    }
+}
+
 export const Products : CollectionConfig = {
     slug : "products",
     admin : {
         useAsTitle: "name"
     },
-    access : {},
+    access : {
+        read: isAdminOrHasAccess(),
+        update: isAdminOrHasAccess(),
+        delete: isAdminOrHasAccess(),
+    },
     hooks:{
-        beforeChange:[
+        afterChange: [syncUser],
+        beforeChange: [
             addUser,
             async (args) => {
                 if(args.operation==="create"){
--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { IncomingMessage } from "http";
 import { stripeWebhookHandler } from "./webhooks";
 import nextBuild from "next/dist/build"
 import path from "path";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000
@@ -40,6 +42,23 @@ const start = async () => {
             }
         }
     })
+
+    const cartRouter = express.Router()
+
+    cartRouter.use(payload.authenticate)
+
+    cartRouter.get("/",(req,res) => {
+        const request = req as PayloadRequest
+
+        if(!request.user) return res.redirect("/sign-in?origin=cart")
+
+        const parsedUrl = parse(req.url, true)
+
+        return nextApp.render(req, res, "/cart", parsedUrl.query)
+    })
+
+
+    app.use("/cart", cartRouter)
 
     if(process.env.NEXT_BUILD)
     {
